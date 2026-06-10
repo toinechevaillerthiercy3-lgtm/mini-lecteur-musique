340526e6037ff3b5801a9a787b53baa64aa059b4
toinechevaillerthiercy3-lgtm <toinechevaillerthiercy3@gmail.com>
2026-06-10 13:40:11 +0200

    Refactor playlist management and rendering logic

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let playlist = JSON.parse(localStorage.getItem("playlist")) || [];
+let playlists = JSON.parse(localStorage.getItem("playlists")) || {
+    "Playlist 1": [],
+    "Playlist 2": []
+};
+
+let currentPlaylist = "Playlist 1";
 
 function addVideo() {
     const input = document.getElementById("youtubeUrl");
@@ -11,11 +16,22 @@ function addVideo() {
         return;
     }
 
-    playlist.push(videoId);
-    localStorage.setItem("playlist", JSON.stringify(playlist));
+    // 🔥 choix playlist
+    let choice = prompt("Dans quelle playlist ? (Playlist 1 / Playlist 2)");
+
+    if (!choice) return;
+
+    if (!playlists[choice]) {
+        alert("Playlist inexistante !");
+        return;
+    }
+
+    playlists[choice].push(videoId);
+
+    localStorage.setItem("playlists", JSON.stringify(playlists));
 
     input.value = "";
-    renderPlaylist();
+    renderPlaylist(choice);
 }
 
 function extractVideoId(url) {
@@ -25,19 +41,22 @@ function extractVideoId(url) {
 
 function playVideo(id) {
     document.getElementById("player").src =
-        "https://www.youtube.com/embed/" + id;
+        "https://www.youtube.com/embed/" + id + "?autoplay=1";
 }
 
-function renderPlaylist() {
+function renderPlaylist(name = "Playlist 1") {
     const list = document.getElementById("playlist");
     list.innerHTML = "";
 
-    playlist.forEach((id, index) => {
+    currentPlaylist = name;
+
+    playlists[name].forEach((id, index) => {
         const li = document.createElement("li");
-        li.textContent = "🎵 Musique " + (index + 1);
+        li.textContent = name + " - Musique " + (index + 1);
         li.onclick = () => playVideo(id);
         list.appendChild(li);
     });
 }
 
+// affichage initial
 renderPlaylist();
